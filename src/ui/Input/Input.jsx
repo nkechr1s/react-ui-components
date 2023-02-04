27f2888e10e4ef295d37216e8cf79ee568e0c54c
Nikos Kechris <nkechris22@gmail.com>
2023-02-04 14:55:36 +0200

    Fix

--- src/ui/Input/Input.jsx
+++ src/ui/Input/Input.jsx
@@ -16,8 +16,7 @@ const Input = ({
       <input
         type={type}
         className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1
-         border-gray-300 appearance-none  
-         focus:outline-none focus:ring-0 focus:border-blue-600 peer border"
+         border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer border"
         onChange={onChange}
         placeholder={placeHolder}
         aria-label={ariaLabel}
@@ -29,15 +28,25 @@ const Input = ({
       />
       {id &&
         (required ? (
-          <label htmlFor={id} className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
+          <label
+            htmlFor={id}
+            className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
            scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600
            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-           peer-focus:-translate-y-4 left-1">{name} *</label>
+           peer-focus:-translate-y-4 left-1"
+          >
+            {name} *
+          </label>
         ) : (
-          <label htmlFor={id} className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
+          <label
+            htmlFor={id}
+            className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
           scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 
           peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-          peer-focus:-translate-y-4 left-1">{name}</label>
+          peer-focus:-translate-y-4 left-1"
+          >
+            {name}
+          </label>
         ))}
     </div>
   );
