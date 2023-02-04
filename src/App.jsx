81e24b16ac2d976fabaf5c785339d5c8b58f2ed7
Nikos Kechris <nkechris22@gmail.com>
2023-02-04 14:15:30 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Input from "./ui/Input/Input";
 const App = () => {
   return (
-    <div className="flex flex-col justify-center content-center flex-wrap w-full absolute items-start mx-auto pb-16">
+    <div className="flex flex-col justify-center content-center flex-wrap w-full  items-start mx-auto pb-16">
       <Input
         type="text"
         ariaLabel="name"
