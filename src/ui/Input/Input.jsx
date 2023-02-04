d3811b79f4545fcc730c3a9e6b4c80d8c902a216
Nikos Kechris <nkechris22@gmail.com>
2023-02-04 15:18:13 +0200

    Make required simplier

--- src/ui/Input/Input.jsx
+++ src/ui/Input/Input.jsx
@@ -26,28 +26,17 @@ const Input = ({
         label={label}
         required={required}
       />
-      {id &&
-        (required ? (
-          <label
-            htmlFor={id}
-            className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
+      {id && (
+        <label
+          htmlFor={id}
+          className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
            scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600
            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
            peer-focus:-translate-y-4 left-1"
-          >
-            {label} *
-          </label>
-        ) : (
-          <label
-            htmlFor={id}
-            className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
-          scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 
-          peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-          peer-focus:-translate-y-4 left-1"
-          >
-            {label}
-          </label>
-        ))}
+        >
+          {label} {required &&("*")}
+        </label>
+      )}
     </div>
   );
 };
