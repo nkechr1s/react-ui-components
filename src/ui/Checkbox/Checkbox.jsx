0be33fe2180218e46f1c8f728150e9b01f5e96db
Nikos Kechris <nkechris22@gmail.com>
2023-02-07 00:59:42 +0200

    More optimaization

--- src/ui/Checkbox/Checkbox.jsx
+++ src/ui/Checkbox/Checkbox.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Checkbox = ({ label, required, linkTitle, ...props }) => {
+const Checkbox = ({linkTitle,required, ...props }) => {
   return (
     <div className="relative mb-2 w-full">
       <label className="text-gray-400 flex gap-1 items-center">
         <input type="checkbox" {...props} required={required} />
-        {label}
+        {props.label}
         {props.href && (
           <a className="underline" href={props.href}>
             {linkTitle}
--- src/ui/Textarea/Textarea.jsx
+++ src/ui/Textarea/Textarea.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-const Textarea = ({ id, label, required, ...props }) => {
+const Textarea = ({required, ...props }) => {
   return (
     <div className="relative mb-2 w-full">
       <textarea
         className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1
         border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer border"
-        id={id}
+        id={props.id}
         required={required}
         {...props}
       />
-      {id && (
+      {props.id && (
         <label
-          htmlFor={id}
+          htmlFor={props.id}
           className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
            scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600
            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-[25px] peer-focus:top-2 peer-focus:scale-75 
            peer-focus:-translate-y-4 left-1"
         >
-          {label} {required && "*"}
+          {props.label} {required && "*"}
         </label>
       )}
     </div>
