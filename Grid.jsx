25fb584fc8e5d873234b2f99cdf3b4f7b80a4f33
Nikos Kechris <nkechris22@gmail.com>
2023-02-18 22:57:52 +0200

    Create grid

---
+++ Grid.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Grid() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => {
+        setBlogs(response.data.slice(0, 5));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-5 gap-1 sm:grid-cols-1 sm:grid-flow-row sm:auto-rows-max">
+      {blogs.length > 0 && (
+        <div className="col-span-1 bg-red-300">{blogs[0].title}</div>
+      )}
+      <div className="col-span-4 grid grid-cols-2 gap-1 sm:grid-cols-4 sm:grid-flow-column">
+        {blogs.slice(1, 5).map((item, index) => (
+          <div
+            key={index}
+            className={`bg-red-300 ${index < 2 ? "row-1" : "row-2"}`}
+          >
+            {item.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Button from "./ui/Buttons/Button";
 import { ReactComponent as EyeIcon } from "./assets/eye.svg";
 import { ReactComponent as EyeIconHide } from "./assets/eyeHide.svg";
 import Checkbox from "./ui/Checkbox/Checkbox";
+import Grid from "../Grid";
 const App = () => {
   const [passwordShown, setPasswordShown] = useState();
   const [isChecked, setIsChecked] = useState();
@@ -15,65 +16,9 @@ const App = () => {
     setIsChecked(!isChecked);
   };
   return (
-    <form className="flex flex-col justify-center content-center flex-wrap w-[900px]  items-start mx-auto pb-16 pt-16">
-      <Input
-        type="text"
-        aria-label="name"
-        placeholder=" "
-        id="test"
-        label="name"
-      />
-      <Input
-        type="email"
-        aria-label="email"
-        placeholder=" "
-        id="email"
-        label="email"
-        required
-      />
-
-      <div className="relative w-full">
-        <Input
-          type={passwordShown ? "text" : "password"}
-          arial-abel="password"
-          placeholder=" "
-          id="password"
-          label="password"
-          minLength="8"
-          required
-        />
-        <div
-          className="mb-2 absolute block top-[10px] right-[10px] translate-x-0 translate-y-[50%] cursor-pointer"
-          onClick={togglePassword}
-        >
-          {passwordShown ? <EyeIcon /> : <EyeIconHide />}
-        </div>
-      </div>
-
-      <Checkbox
-        className="w-4 h-4 cursor-pointer"
-        label="My value"
-        checked={isChecked}
-        onChange={handleOnChange}
-        href="test"
-        linkTitle="Όροι χρήσης"
-        required
-      />
-      <p className="text-gray-600">
-        Above checkbox is {isChecked ? "checked" : "unchecked"}.
-      </p>
-      <Textarea
-        rows="10"
-        ariainvalid="false"
-        arialabel="email"
-        placeholder=" "
-        id="email"
-        label="email"
-        required
-      />
-      <Button text="Click me" />
-  
-    </form>
+    <div className="flex flex-col justify-center content-center flex-wrap items-start mx-auto pb-16 pt-16">
+      <Grid />
+    </div>
   );
 };
 
